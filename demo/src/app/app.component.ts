32694f3ec6ca6901c1a686c8a01dc068918fc04f
wellwind <wellwind@mail2000.com.tw>
2019-03-22 16:42:24 +0800

    更改目錄名稱

---
+++ demo/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test';
+  subtitle = '記載著 Will 在網路世界的學習心得與<u>技術分享</u>';
+  url = 'https://blog.miniasp.com/';
+  titleLeft = true;
+  defaultKeyword = 'angular';
+  keyword = '';
+
+  search(e: MouseEvent) {
+    if (e.altKey) {
+      this.title = this.title + '!';
+    }
+    console.log(e.altKey);
+  }
+
+  keywordInput(e: KeyboardEvent) {
+    this.keyword = (e.target as HTMLInputElement).value;
+
+  }
+
+  searchKeyword() {
+    // console.log(this.keyword);
+  }
+}
