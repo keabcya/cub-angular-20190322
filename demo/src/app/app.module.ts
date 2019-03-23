3190c8976859f6d4b8ebe7fbeec2f198971e2f98
wellwind <wellwind@mail2000.com.tw>
2019-03-23 09:16:20 +0800

    切出 article component 並使用 Input 傳入資料

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
 
 
 
@@ -12,7 +13,8 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
